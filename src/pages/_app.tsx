d56727233bb437c55d7abfb136149846d1f082e2
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-05-13 16:13:30 -0400

    prettier settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
 
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme
+} from '@mantine/core'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark')
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || colorScheme === 'dark' ? 'dark' : 'light')
+  }
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <Component {...pageProps} />
+    </ColorSchemeProvider>
+  )
 }
